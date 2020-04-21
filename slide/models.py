e1a6a142771e1688ec84d978310b236083fd60ba
hecaiont <it8745@ocmobile.co.kr>
2020-04-21 19:37:51 +0900

    first commit, model, migrate

---
+++ slide/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Achievements(models.Model):
+    title = models.CharField(max_length=100)
+    contents = models.TextField(max_length=500, blank=True)
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
+    visible = models.BooleanField(default=True)
+    highlight = models.BooleanField(default=False)
+    tags = models.TextField(max_length=500, blank=True)
