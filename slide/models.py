a6dcfc04210fad9ff1ecee7045f7062d30b7aa7a
hecaiont <it8745@ocmobile.co.kr>
2020-05-06 18:56:18 +0900

    model migrate, category add, select field, modify views

--- slide/models.py
+++ slide/models.py
@@ -2,7 +2,23 @@ from django.db import models
 
 class Achievement(models.Model):
     title = models.CharField(max_length=100)
-    category = models.CharField(max_length=100) # select field
+
+    category_choice = (
+        ('PR', 'Profile'),
+		('ED', 'Education'),
+        ('EX', 'Experience'),
+        ('WE', 'Work Experience'),
+        ('WH', 'Work History'),
+        ('PS', 'Personal Summary'),
+        ('SU', 'Summary'),
+        ('LA', 'Language'),
+        ('OB', 'Objective'),
+        ('SK', 'Skills'),
+        ('KS', 'Key Skills'),
+        ('AD', 'Additional Infomation'),
+    )
+    category = models.CharField(max_length=2, choices=category_choice)
+    
     contents = models.TextField(max_length=500, blank=True)
     start_date = models.DateField(null=True, blank=True)
     end_date = models.DateField(null=True, blank=True)
@@ -11,7 +27,7 @@ class Achievement(models.Model):
     tags = models.TextField(max_length=500, blank=True)
 
     class Meta:
-        ordering = ['title', 'start_date']
+        ordering = ['start_date']
 
     def __str__(self): # show on admin change list
         return self.title
